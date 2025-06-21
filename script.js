5dd148339b0249749fec514e2b111e46958cd7ec
João Pedro Macena <joaomacenactba@gmail.com>
2025-06-21 15:28:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const copyrightElement = document.getElementById('copyright');
-    const currentYear = new Date().getFullYear();                                                               // Com isso, vai fazer que "pegue" o ano que está no sistema e sempre atualizar
+    const currentYear = new Date().getFullYear();                                                               // Com isso vai atualizar o ano automaticamente no footer
 
     copyrightElement.textContent = `© ${currentYear} João Pedro Macena Correa. Todos os direitos reservados.`;  // Vai atualizra o texto (ano) do rodapé
 });
